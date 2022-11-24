9419865d760b898600232dc0316c0b1049e386fb
xiaoqi.cxq <xiaoqi.cxq@gmail.com>
2022-11-24 16:49:41 +0800

    导出pdf文案调整

--- src/components/modals/PdfExportModal.vue
+++ src/components/modals/PdfExportModal.vue
@@ -1,7 +1,7 @@
 <template>
   <modal-inner aria-label="导出到PDF">
     <div class="modal__content">
-      <p>请为您的<b> pdf导出</b>选择模板。</p>
+      <p>请为您的<b> pdf导出</b>选择模板。(该导出很消耗服务器资源,文档太大或图片太多可能会导出超时失败,限时开放使用!)</p>
       <form-entry label="模板">
         <select class="textfield" slot="field" v-model="selectedTemplate" @keydown.enter="resolve()">
           <option v-for="(template, id) in allTemplatesById" :key="id" :value="id">
